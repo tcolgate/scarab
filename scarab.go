9add719332c61f33806bdc8e3eb4cfac10851f1c
Tristan Colgate <tcolgate@gmail.com>
2021-03-06 14:27:37 +0000

    stub imeplementations

--- scarab.go
+++ scarab.go
@@ -1,10 +1,49 @@
 package scarab
 
-import pb "github.com/tcolgate/scarab/pb"
+import (
+	"log"
+	"time"
+
+	pb "github.com/tcolgate/scarab/pb"
+)
 
 // Server implments the leaders service.
-type Server struct{}
+type Server struct {
+	done chan struct{}
+}
 
-func (*Server) Register(*pb.RegisterRequest, pb.Leader_RegisterServer) error {
+func (s *Server) Register(req *pb.RegisterRequest, stream pb.Leader_RegisterServer) error {
+	ticker := time.NewTicker(1 * time.Second)
+	ctx := stream.Context()
+	defer ticker.Stop()
+
+	log.Printf("registered worker for profile %s", req.Profile)
+
+	for {
+		select {
+		case <-ctx.Done():
+		case <-s.done:
+		case <-ticker.C:
+			stream.Send(&pb.KeepAlive{})
+		}
+	}
+	return nil
+}
+
+func (s *Server) RunJob(req *pb.RunJobRequest, stream pb.Worker_RunJobServer) error {
+	ticker := time.NewTicker(1 * time.Second)
+	ctx := stream.Context()
+	defer ticker.Stop()
+
+	log.Printf("start running profile %s", req.Profile)
+
+	for {
+		select {
+		case <-ctx.Done():
+		case <-s.done:
+		case <-ticker.C:
+			stream.Send(&pb.JobMetrics{})
+		}
+	}
 	return nil
 }
